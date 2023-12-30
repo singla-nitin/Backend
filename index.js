88df1d093ae86683ea8a857db8906118d75afad7
Nitin singla <136202550+singla-nitin@users.noreply.github.com>
2023-12-30 14:17:14 +0000

    written code for express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Setting up environment with node and express")
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get('/',(req,res)=>{
+      res.send("THIS IS HOME PAGE!!!")
+})
+app.get('/login',(req,res)=>{
+res.send("<h1> welcome to login page</h1>")
+})
+app.listen(port,()=>{
+    console.log(`APP LISTENING ON PORT ${port}.`)
+})
